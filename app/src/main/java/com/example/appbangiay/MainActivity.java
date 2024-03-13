b5b383d28724afb84856aa256f0b8df58282b741
manh <manhtvph22328@fpt.edu.vn>
2024-03-14 00:23:28 +0700

    n

--- app/src/main/java/com/example/appbangiay/MainActivity.java
+++ app/src/main/java/com/example/appbangiay/MainActivity.java
@@ -11,8 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String test = "Hello dit con me may";
-        String name = "Hello dit con me may";
-
     }
 }
